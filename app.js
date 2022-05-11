00a39af2b4f4ed0d63323b364b0708eea9d61199
Giselaflores1782 <gisflores@alumno.unlam.edu.ar>
2022-05-11 20:23:53 -0300

    codigo completo

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+// botones
+
+let pagina = 1;
+const btnAnterior = document.getElementById("btnAnterior");
+const btnSiguiente = document.getElementById("btnSiguiente");
+
+btnAnterior.addEventListener("click", () =>{
+	if(pagina > 1){
+		pagina -= 1;
+		cargarPelicula();
+	}
+});
+
+btnSiguiente.addEventListener("click", () =>{
+	if(pagina <= 1000){
+		pagina += 1;
+		cargarPelicula();
+	}
+});
+
 // carga de la API
 
 const cargarPelicula = async() =>{
 	try{
-		const respuesta = await fetch("https://api.themoviedb.org/3/movie/550?api_key=191528030c357419329af1198edbcb24");
+		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=191528030c357419329af1198edbcb24&language=es-MX&page=${pagina}`);
 		console.log(respuesta);
 
 		if(respuesta.status === 200){
@@ -11,14 +31,18 @@ const cargarPelicula = async() =>{
 			let peliculas ="";
 			datos.results.forEach( peliculas => {
 				peliculas += `<div class="pelicula">
-							  <img class="poster" scr=""
-
+							  <img class="poster" scr="https://image.tmdb.org/t/p/w500/${peliculas.poster_path}">
+							  <h3 class="titulo">${peliculas.title} </h3>
 							  </div>
-				
-				`;
-				
+							  `;
 			});
+
+			document.getElementById("contenedor").innerHTML = peliculas;
 		}
+
+		else if(respuesta.status === 401){ console.log("No tiene la api_key correcta");}
+		else if(respuesta.status === 404){ console.log("Lo buscado no esta disponible");}
+		else {console.log("Hubo un error desconocido y no sabemos de donde viene");}
 
 	} catch(error){
 		console.log(error.messsage);
