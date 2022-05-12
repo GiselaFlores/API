f94c3cd57cfa572f4aeb7402991a1d2f6833ab5d
Giselaflores1782 <gisflores@alumno.unlam.edu.ar>
2022-05-11 22:27:23 -0300

    modificado

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
 // botones
 
 let pagina = 1;
-const btnAnterior = document.getElementById("btnAnterior");
-const btnSiguiente = document.getElementById("btnSiguiente");
+const btnAnterior = document.getElementById('btnAnterior');
+const btnSiguiente = document.getElementById('btnSiguiente');
 
-btnAnterior.addEventListener("click", () =>{
-	if(pagina > 1){
-		pagina -= 1;
-		cargarPelicula();
+btnSiguiente.addEventListener("click", () => {
+	if(pagina < 1000){
+		pagina += 1;
+		cargarPeliculas();
 	}
 });
 
-btnSiguiente.addEventListener("click", () =>{
-	if(pagina <= 1000){
-		pagina += 1;
-		cargarPelicula();
+btnAnterior.addEventListener("click", () => {
+	if(pagina > 1){
+		pagina -= 1;
+		cargarPeliculas();
 	}
 });
 
 // carga de la API
 
-const cargarPelicula = async() =>{
+const cargarPeliculas = async() =>{
 	try{
 		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=191528030c357419329af1198edbcb24&language=es-MX&page=${pagina}`);
 		console.log(respuesta);
 
 		if(respuesta.status === 200){
 			const datos = await respuesta.json();
-
-			let peliculas ="";
-			datos.results.forEach( peliculas => {
-				peliculas += `<div class="pelicula">
-							  <img class="poster" scr="https://image.tmdb.org/t/p/w500/${peliculas.poster_path}">
-							  <h3 class="titulo">${peliculas.title} </h3>
-							  </div>
-							  `;
+			
+			let peliculas = "";
+			datos.results.forEach(pelicula => {
+				peliculas += `
+					<div class="pelicula">
+						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+						<h3 class="titulo">${pelicula.title}</h3>
+					</div>
+				`;
 			});
 
-			document.getElementById("contenedor").innerHTML = peliculas;
+			document.getElementById('contenedor').innerHTML = peliculas;
 		}
 
 		else if(respuesta.status === 401){ console.log("No tiene la api_key correcta");}
@@ -49,4 +50,4 @@ const cargarPelicula = async() =>{
 	}
 }
 
-cargarPelicula();
+cargarPeliculas();
